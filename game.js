9324fba8de6fe779a92753e41326602b3042f82f
max <maxmac@maxs-MacBook-Pro.local>
2017-05-12 11:35:50 -0700

    we gonna go hard now

---
+++ game.js
@@ -0,0 +1,14 @@
+class Game
+{ 
+    
+    constructor()
+    { 
+        this.players = {};
+        this.id;
+    }
+    
+    playerJoin(player)
+    {
+        players.add(player);      
+    }
+}
