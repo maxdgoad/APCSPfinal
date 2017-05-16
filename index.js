b31c1ff45dc14ab8561beca6e042695b3e842695
Richie Zins <rizins@MacBookAir.local>
2017-05-16 10:15:52 -0700

    Orginazation

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+//Require in this context is a node.js function similar to importing a class in java or defining it in a script tag in an html webpage
+// Import the Express module
+var express = require('express');
+
+// Import the path module (packaged with Node.js)
+var path = require('path');
+
+// Create a new instance of Express
+var app = express();
+
+// Create a simple Express application
+app.configure(function() {
+    // Turn down the logging activity
+    //Dont really know how this works just read online to do this way to obtain less met data about user
+    app.use(express.logger('dev'));
+    // Serve static html, js, css, and image files from the public 
+    app.use(express.static(path.join(__dirname,'public')));
+ });
+
+// Create a Node.js based http server on port 6969
+var server = require('http').createServer(app).listen(6969);
+
+// Create a Socket.IO server and attach it to the http server
+var io = require('socket.io').listen(server);
+
+// Import the memeGameBackend game file.
+var mgb = require('./memeGameBackend');
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {
+    //console.log('client connected');
+    mgb.createGame(io, socket);
+});
 
--- memeGameBackend.js
+++ memeGameBackend.js
@@ -1,1 +1,14 @@
+var io;
+var gameSocket;
+
+// This function is called by index.js to initialize a new game instance.
+//sio The Socket.IO library
+//socket The socket object for the connected client.
+ 
+exports.createGame = function(sio, socket){
+    io = sio;
+    gameSocket = socket;
+    gameSocket.emit('connected', { message: "You are connected!" });
+    console.log("connected")
+}
 
