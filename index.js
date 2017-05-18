ba128592c930e01d2b1948e2b5b1a673a57aa930
Max Goad <maxdgoad@gmail.com>
2017-05-18 10:21:57 -0700

    fixed things

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ var path = require('path');
 var app = express();
 
 // Create a simple Express application
-app.configure(function() {
+
     // Turn down the logging activity
     //Dont really know how this works just read online to do this way to obtain less met data about user
-    app.use(express.logger('dev'));
     // Serve static html, js, css, and image files from the public 
     app.use(express.static(path.join(__dirname,'public')));
- });
 
 // Create a Node.js based http server on port 6969
 var server = require('http').createServer(app).listen(6969);
+console.log("what on 6969");
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
@@ -30,5 +29,6 @@ var mgb = require('./memeGameBackend');
 io.sockets.on('connection', function (socket) {
     //console.log('client connected');
     mgb.createGame(io, socket);
+    console.log("what");
 });
 
