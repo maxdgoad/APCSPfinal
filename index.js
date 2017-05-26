8634e662d49cb8f80bcc09836324b58712468aac
RIZY101 <rizins@zins.dynu.com>
2017-05-26 10:46:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+//Require in this context is a node.js function similar to importing a class in java or defining it in a script tag in an html webpage
+// Import the Express module
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
 var express = require('express');
-var mgb = require( path.resolve( __dirname, "./memeGameBackend" ));
+var path = require('path');
+var server = require('http').Server(app);
+// Create a Socket.IO server and attach it to the http server
+var io = require('socket.io')(server);
+//a Node.js based http server on port 6969
+server.listen(6969);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use(express.static(path.join(__dirname, '/')));
+// Import the memeGameBackend game file.
+var mgb = require('./memeGameBackend');
 
-
-io.on('connection', function(socket){
-    mgb.creategamex(io, socket);
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {
+    console.log('client connected');
+    mgb.createGame(io, socket);
 });
 
-http.listen(6969, function(){
-  console.log('port 6969');
-});
+
